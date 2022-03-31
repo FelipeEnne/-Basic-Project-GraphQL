cc152b5e1d17b2308d78991d8214b4bd68465353
FelipeEnne <felipeenne@gmail.com>
2022-03-31 17:28:09 -0300

    finish backend

--- backend/migrations/20190411112543_tabela_perfis.js
+++ backend/migrations/20190411112543_tabela_perfis.js
@@ -1,17 +1,18 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('perfis', table => {
-      table.increments('id').primary()
-      table.string('nome').notNull().unique()
-      table.string('rotulo').notNull()
-  }).then(function () {
-      return knex('perfis').insert([
-          { nome: 'comum', rotulo: 'Comum' },
-          { nome: 'admin', rotulo: 'Administrador' },
-      ])
-  })
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .createTable("perfis", (table) => {
+      table.increments("id").primary();
+      table.string("nome").notNull().unique();
+      table.string("rotulo").notNull();
+    })
+    .then(function () {
+      return knex("perfis").insert([
+        { nome: "comum", rotulo: "Comum" },
+        { nome: "admin", rotulo: "Administrador" },
+      ]);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('perfis')
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable("perfis");
 };
--- backend/resolvers/Type/Perfil.js
+++ backend/resolvers/Type/Perfil.js
@@ -1,13 +1,9 @@
-const db = require('../../config/db')
+const db = require("../../config/db");
 
 module.exports = {
-    usuarios(perfil) {
-        return db('usuarios')
-            .join(
-                'usuarios_perfis',
-                'usuarios.id',
-                'usuarios_perfis.usuario_id'
-            )
-            .where({ perfil_id: perfil.id })
-    }
-}
+  usuarios(perfil) {
+    return db("usuarios")
+      .join("usuarios_perfis", "usuarios.id", "usuarios_perfis.usuario_id")
+      .where({ perfil_id: perfil.id });
+  },
+};
